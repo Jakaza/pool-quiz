bd42cb1344a8e8f809923eaa1a4f778deccacf21
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-01-31 11:07:57 +0100

    Fix question url generator questions query     *Use aggregate to get random collection

--- quiz-routes/api/question.js
+++ quiz-routes/api/question.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const StatusCodes = require('../../constants/StatusCodes')
+const TrueFalseQuestion = require('../../models/trueFalseQuestion')
 const MultipleChoiceQuestion = require('../../models/multipleChoiceQuestion')
-const TrueFalseQuestion = require('../../models/TrueFalseQuestion')
-
 
 // https://opentdb.com/api.php?amount=10&category=23&difficulty=hard&type=multiple
 
 router.get('/generate-url', async (req, res )=>{
-    let url = 'http://localhost:3000/api/v1/question?'
-
-    // Construct query parameters
+    let url = 'http://localhost:3000/api/v1/question'
     const queryParams = [];
-    if (req.query.amount) queryParams.push('amount=' + encodeURIComponent(req.query.amount));
-    if (req.query.category) queryParams.push('category=' + encodeURIComponent(req.query.category));
-    if (req.query.difficulty) queryParams.push('difficulty=' + encodeURIComponent(req.query.difficulty));
-    if (req.query.type) queryParams.push('type=' + encodeURIComponent(req.query.type));
-
+    if (req.body.amount) queryParams.push('amount=' + encodeURIComponent(req.body.amount));
+    if (req.body.category) queryParams.push('category=' + encodeURIComponent(req.body.category));
+    if (req.body.difficulty) queryParams.push('difficulty=' + encodeURIComponent(req.body.difficulty));
+    if (req.body.type) queryParams.push('type=' + encodeURIComponent(req.body.type));
     if (queryParams.length > 0) {
         url += '?' + queryParams.join('&');
     }
-
     res.status(StatusCodes.Created).json({
         status: true, 
         message: 'Url has been successfully created.',
@@ -27,41 +23,43 @@ router.get('/generate-url', async (req, res )=>{
     })
 })
 
-
-router.get('/question', async (req, res )=>{
-        // Build the query object based on query parameters
+router.get('/questions', async (req, res )=>{
         const query = {};
-        if (req.query.amount) query.amount = req.query.amount;
         // if (req.query.category) query.category = req.query.category;
-        // if (req.query.difficulty) query.difficulty = req.query.difficulty;
-        // if (req.query.type) query.type = req.query.type;
+        if (req.query.difficulty) query.difficulty = req.query.difficulty;
 
-        const limitAmount = parseInt(req.query.amount, 10);
+        const amount = req.query.amount? req.query.amount : 10
+        let limitAmount = parseInt(amount, 10);
         const response = {
             status: true,
-            url: 'http://localhost:3000/api/v1/question?',
             data : [
             ]
         }
         try{
             if(isDifficultyChecked(req)){
                 if(req.query.type == "multiple"){
-                    const data = await MultipleChoiceQuestion.find().limit(limitAmount)
+                    const data = await MultipleChoiceQuestion.aggregate([
+                        { $sample: { size: limitAmount } }
+                      ])
                     response.data.push(data)
                 }else{
-                    const data = await TrueFalseQuestion.find().limit(limitAmount)
+                    const data = await TrueFalseQuestion.aggregate([
+                        { $sample: { size: limitAmount } }
+                      ])
                     response.data.push(data)
                 }
             }else{
                 limitAmount = Math.floor(limitAmount/2)
-                const data = await MultipleChoiceQuestion.find().limit(limitAmount)
-                const data2 = await TrueFalseQuestion.find().limit(limitAmount)
+                const data = await MultipleChoiceQuestion.aggregate([
+                    { $sample: { size: limitAmount } }
+                  ])
+                const data2 = await TrueFalseQuestion.aggregate([
+                    { $sample: { size: limitAmount } }
+                  ])
                 response.data.push(data)
                 response.data.push(data2)
             }
-
             res.json(response)
-
         }catch(er){
             console.log(er);
         }
@@ -70,6 +68,4 @@ router.get('/question', async (req, res )=>{
 function isDifficultyChecked(req){
     return req.query.difficulty ? true : false
 }
-
-
 module.exports = router;
