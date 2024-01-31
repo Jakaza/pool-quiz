282b1b6b3f7ecc59b4e3395bab9cdcbfd7d4380a
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-01-31 12:48:55 +0100

    Implement user interface - index page     add route to index page

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/testing', (req, res) =>{
+app.get('/', (req, res) =>{
     res.render('index')
 })
 
