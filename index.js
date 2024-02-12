3520734e580e34df590c09451b8e273174d4d0e3
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-02-12 15:05:43 +0100

    Remove Some Lines

--- index.js
+++ index.js
@@ -5,38 +5,23 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const app = express()
 const routes = require('./routes')
-const apiRoutes = require('./quiz-routes/index')
 const initDB = require('./config/connectDB')
 const seedDB = require('./config/seed')
 const passport = require('passport')
-
 initDB().then(() => {
     // seedDB()
 });
-       
-
 //Middlewares Config
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
-
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
-
-
 require('./config/passport')
 app.use(passport.initialize());
 app.use(routes)
-app.use(apiRoutes)
-
-
-
-
 const server = http.createServer(app)
-
 const PORT = process.env.PORT || 3000
 server.listen(PORT, ()=>{
     console.log(`Server is up at port ${PORT}`);
