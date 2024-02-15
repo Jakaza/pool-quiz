675f06f4857ba3e13d4e8a9569857a2887b98481
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-02-15 20:20:50 +0100

    set up SuperAdmin functionality structure

---
+++ controllers/admin.js
@@ -0,0 +1,10 @@
+const SuperAdmin = {
+    publishQuestion: {},
+    unPublishQuestion: {},
+    blockUser: {},
+    unBlockUser: {},
+    upDateUser: {},
+    makeUserAdmin: {}
+}
+
+module.exports = SuperAdmin
